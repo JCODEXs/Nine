2c1a9346efef61115fee4e9f3adeb33dbb863898
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-06-25 16:03:21 -0500

    wip

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,9 +1,6 @@
-import { connectToDatabase } from "@/lib/mongoDb"
+
 
 export async function GET(req ){
-  const client= await connectToDatabase()
-   console.log(client)
-  
 
   return new Response (JSON.stringify({message:'Hello, Next.js!'}))
 }
